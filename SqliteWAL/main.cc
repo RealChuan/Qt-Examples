af536323989ef862bb7e2a02d142c5586bbb7451
浮生若梦 <1070753498@qq.com>
2023-11-03 18:41:09 +0800

    [优化SqliteWAL示例，增强并发插入性能]: 引入了CountDownLatch同步辅助类，优化了多线程插入数据库的性能。
    - 在`main.cc`中，新增了`CountDownLatch`类，用于控制多线程同时开始执行插入操作，以增强并发性能。
    - 修改了`insertThread`函数，接受`CountDownLatch`作为参数，并在所有线程准备好后统一开始插入操作。
    - 更新了主函数`main`，移除了对`QSqlDatabase::drivers`的检查，因为`sqlutils.cc`中已有类似逻辑。
    - 在`main`函数中，使用`std::ref`传递`CountDownLatch`对象到线程函数，并在所有线程启动后调用`countDown`方法。
    - 在`sqlutils.cc`中，移除了`checkSQLITEDriver`函数，因为现在驱动检查在`main.cc`中处理。
    - 更新了`SqlUtilsPrivate`构造函数，直接调用`openSqliteDatabase`方法，去掉了对`checkSQLITEDriver`的调用。

--- SqliteWAL/main.cc
+++ SqliteWAL/main.cc
@@ -1,12 +1,50 @@
 #include "sqlutils.hpp"
 
 #include <QCoreApplication>
+#include <QMutex>
+#include <QSqlDatabase>
+#include <QWaitCondition>
 
 #include <thread>
 
-void insertThread(const QString &brand)
+class CountDownLatch
+{
+    Q_DISABLE_COPY_MOVE(CountDownLatch)
+public:
+    explicit CountDownLatch(int count)
+        : m_count(count)
+    {}
+    void countDown()
+    {
+        QMutexLocker lock(&m_mutex);
+        if (--m_count == 0) {
+            m_condition.wakeAll();
+        }
+    }
+    int count() const
+    {
+        QMutexLocker lock(&m_mutex);
+        return m_count;
+    }
+
+    void wait()
+    {
+        QMutexLocker lock(&m_mutex);
+        while (m_count > 0) {
+            m_condition.wait(&m_mutex);
+        }
+    }
+
+private:
+    mutable QMutex m_mutex;
+    QWaitCondition m_condition;
+    int m_count;
+};
+
+void insertThread(const QString &brand, CountDownLatch &countDown)
 {
     SqlUtils sqlUtils;
+    countDown.wait();
     for (int i = 0; i < 500; i++) {
         sqlUtils.insert(brand, i);
     }
@@ -16,15 +54,22 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    SqlUtils sqlUtils;
+    if (!QSqlDatabase::drivers().contains("QSQLITE")) {
+        qWarning() << "This demo needs the SQLITE driver";
+        return -1;
+    }
 
     const QStringList brands{"Apple", "Samsung", "Xiaomi", "Huawei", "Oppo", "Vivo", "Realme"};
+    CountDownLatch countDown(1);
     std::vector<std::thread> threads;
     for (const auto &brand : brands) {
-        threads.emplace_back(insertThread, brand);
+        threads.emplace_back(insertThread, brand, std::ref(countDown));
     }
+    countDown.countDown();
     for (auto &thread : threads) {
-        thread.join();
+        if (thread.joinable()) {
+            thread.join();
+        }
     }
 
     return 0;
